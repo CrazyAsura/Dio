a137ea961fb1e82118decd76f9a9c20ea040283d
Leon Mendonça Trindade <136998012+CrazyAsura@users.noreply.github.com>
2024-06-08 21:25:27 -0300

    Delete Python/projeto-1 directory

--- Python/projeto-1/print_input.py
+++
@@ -1,7 +0,0 @@
-nome = input("informe o seu nome: ")
-idade = input("informe a sua idade: ")
-
-print(nome , idade)
-print(nome, idade, end=".../n")
-input(nome, idade, sep="#", end=".../n")
-input(nome, idade, sep="#")
